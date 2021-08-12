f49b2bc6f950bfb47aabbea7478927c164f896fe
vipinkrishna131 <vipinkrishna131@gmail.com>
2021-08-12 16:01:28 +0530

    Code cleaning

--- lib/Home/home.dart
+++ lib/Home/home.dart
@@ -1,5 +1,4 @@
 import 'package:bmi_calculator/Home/home_controller.dart';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
